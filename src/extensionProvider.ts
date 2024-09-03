70a4c86d82125f40a02ad6ee27b86f599ba5cafd
cipriandraghici <cipriandraghici93@gmail.com>
2024-09-03 12:53:41 +0300

    standardize providers

--- src/extensionProvider.ts
+++ src/extensionProvider.ts
@@ -1,11 +1,11 @@
-import { IPlainTransactionObject } from "@multiversx/sdk-core/out";
-import { SignableMessage } from "@multiversx/sdk-core/out/signableMessage";
+import { IPlainTransactionObject, Message } from "@multiversx/sdk-core";
 import { Transaction } from "@multiversx/sdk-core/out/transaction";
 import {
   ErrAccountNotConnected,
   ErrCannotSignSingleTransaction,
 } from "./errors";
 import { Operation } from "./operation";
+import {IDAppProviderAccount, IDAppProviderBase} from "@multiversx/sdk-dapp-utils/out";
 
 declare global {
   interface Window {
@@ -13,14 +13,8 @@ declare global {
   }
 }
 
-interface IExtensionAccount {
-  address: string;
-  name?: string;
-  signature?: string;
-}
-
-export class ExtensionProvider {
-  public account: IExtensionAccount = { address: "" };
+export class ExtensionProvider implements IDAppProviderBase {
+  public account: IDAppProviderAccount = { address: "" };
   private initialized: boolean = false;
   private static _instance: ExtensionProvider = new ExtensionProvider();
 
@@ -54,7 +48,7 @@ export class ExtensionProvider {
       callbackUrl?: string;
       token?: string;
     } = {}
-  ): Promise<string> {
+  ): Promise<IDAppProviderAccount> {
     if (!this.initialized) {
       throw new Error(
         "Extension provider is not initialised, call init() first"
@@ -63,7 +57,7 @@ export class ExtensionProvider {
     const { token } = options;
     const data = token ? token : "";
     await this.startBgrMsgChannel(Operation.Connect, data);
-    return this.account.address;
+    return this.account;
   }
 
   async logout(): Promise<boolean> {
@@ -95,13 +89,16 @@ export class ExtensionProvider {
     return this.account ? this.account.address : "";
   }
 
-  isInitialized(): boolean {
+  isInitialized = (): boolean => {
     return this.initialized;
+  };
+
+  isConnected(): boolean {
+    return Boolean(this.account.address);
   }
 
-  // TODO: In V3, this will not be an async function anymore.
-  async isConnected(): Promise<boolean> {
-    return Boolean(this.account.address);
+  getAccount(): IDAppProviderAccount | null {
+    return this.account;
   }
 
   async signTransaction(transaction: Transaction): Promise<Transaction> {
@@ -147,12 +144,12 @@ export class ExtensionProvider {
     }
   }
 
-  async signMessage(message: SignableMessage): Promise<SignableMessage> {
+  async signMessage(messageToSign: string): Promise<Message> {
     this.ensureConnected();
 
     const data = {
       account: this.account.address,
-      message: message.message.toString(),
+      message: messageToSign,
     };
     const extensionResponse = await this.startBgrMsgChannel(
       Operation.SignMessage,
@@ -161,7 +158,11 @@ export class ExtensionProvider {
     const signatureHex = extensionResponse.signature;
     const signature = Buffer.from(signatureHex, "hex");
 
-    message.applySignature(signature);
+    const message = new Message({
+      data: Buffer.from(messageToSign)
+    });
+    message.signature = signature;
+
     return message;
   }
 
